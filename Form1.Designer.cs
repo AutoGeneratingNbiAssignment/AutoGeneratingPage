673d3dfe48a1caeb81994d86a46ce2a85e5bc942
Dani-dvp <kevin1995dani@gmail.com>
2021-11-03 13:54:53 +0100

    Fixed the layout for the middle part of the forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,15 @@ namespace AutoGeneratingPage
         private void InitializeComponent()
         {
             this.bannerCheckBox = new System.Windows.Forms.CheckBox();
-            this.bannerTextBox = new System.Windows.Forms.TextBox();
-            this.callToActionText = new System.Windows.Forms.TextBox();
             this.ctaFirstBtnText = new System.Windows.Forms.TextBox();
             this.ctaScndBtnText = new System.Windows.Forms.TextBox();
             this.ctaThrdBtnText = new System.Windows.Forms.TextBox();
             this.bannerAddPicture = new System.Windows.Forms.Button();
             this.dropDownTxtPos = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.scrllBannerLabel = new System.Windows.Forms.Label();
+            this.ctaBtnsLabel = new System.Windows.Forms.Label();
+            this.txtWithImagePos = new System.Windows.Forms.Label();
+            this.imgText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // bannerCheckBox
@@ -48,22 +49,6 @@ namespace AutoGeneratingPage
             this.bannerCheckBox.Size = new System.Drawing.Size(15, 14);
             this.bannerCheckBox.TabIndex = 0;
             this.bannerCheckBox.UseVisualStyleBackColor = true;
-            // 
-            // bannerTextBox
-            // 
-            this.bannerTextBox.Location = new System.Drawing.Point(12, 128);
-            this.bannerTextBox.Name = "bannerTextBox";
-            this.bannerTextBox.Size = new System.Drawing.Size(94, 23);
-            this.bannerTextBox.TabIndex = 1;
-            this.bannerTextBox.Text = "Scrolling Banner";
-            // 
-            // callToActionText
-            // 
-            this.callToActionText.Location = new System.Drawing.Point(198, 128);
-            this.callToActionText.Name = "callToActionText";
-            this.callToActionText.Size = new System.Drawing.Size(94, 23);
-            this.callToActionText.TabIndex = 2;
-            this.callToActionText.Text = "CTA Buttons";
             // 
             // ctaFirstBtnText
             // 
@@ -95,41 +80,72 @@ namespace AutoGeneratingPage
             // bannerAddPicture
             // 
             this.bannerAddPicture.ForeColor = System.Drawing.Color.Blue;
-            this.bannerAddPicture.Location = new System.Drawing.Point(22, 175);
+            this.bannerAddPicture.Location = new System.Drawing.Point(21, 177);
             this.bannerAddPicture.Name = "bannerAddPicture";
             this.bannerAddPicture.Size = new System.Drawing.Size(75, 23);
             this.bannerAddPicture.TabIndex = 6;
             this.bannerAddPicture.Text = "Browse";
             this.bannerAddPicture.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // dropDownTxtPos
             // 
             this.dropDownTxtPos.FormattingEnabled = true;
-            this.dropDownTxtPos.Location = new System.Drawing.Point(427, 164);
-            this.dropDownTxtPos.Name = "comboBox1";
+            this.dropDownTxtPos.Location = new System.Drawing.Point(427, 157);
+            this.dropDownTxtPos.Name = "dropDownTxtPos";
             this.dropDownTxtPos.Size = new System.Drawing.Size(121, 23);
             this.dropDownTxtPos.TabIndex = 7;
             // 
-            // textBox1
+            // scrllBannerLabel
             // 
-            this.textBox1.Location = new System.Drawing.Point(427, 128);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 8;
+            this.scrllBannerLabel.AutoSize = true;
+            this.scrllBannerLabel.Location = new System.Drawing.Point(12, 128);
+            this.scrllBannerLabel.Name = "scrllBannerLabel";
+            this.scrllBannerLabel.Size = new System.Drawing.Size(93, 15);
+            this.scrllBannerLabel.TabIndex = 9;
+            this.scrllBannerLabel.Text = "Scrolling Banner";
+            this.scrllBannerLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ctaBtnsLabel
+            // 
+            this.ctaBtnsLabel.AutoSize = true;
+            this.ctaBtnsLabel.Location = new System.Drawing.Point(198, 128);
+            this.ctaBtnsLabel.Name = "ctaBtnsLabel";
+            this.ctaBtnsLabel.Size = new System.Drawing.Size(72, 15);
+            this.ctaBtnsLabel.TabIndex = 10;
+            this.ctaBtnsLabel.Text = "CTA Buttons";
+            // 
+            // txtWithImagePos
+            // 
+            this.txtWithImagePos.AutoSize = true;
+            this.txtWithImagePos.Location = new System.Drawing.Point(427, 128);
+            this.txtWithImagePos.Name = "txtWithImagePos";
+            this.txtWithImagePos.Size = new System.Drawing.Size(163, 15);
+            this.txtWithImagePos.TabIndex = 11;
+            this.txtWithImagePos.Text = "Text With Image And Position";
+            // 
+            // imgText
+            // 
+            this.imgText.Location = new System.Drawing.Point(427, 193);
+            this.imgText.Multiline = true;
+            this.imgText.Name = "imgText";
+            this.imgText.Size = new System.Drawing.Size(242, 88);
+            this.imgText.TabIndex = 12;
+            this.imgText.Text = "Write your text here";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.imgText);
+            this.Controls.Add(this.txtWithImagePos);
+            this.Controls.Add(this.ctaBtnsLabel);
+            this.Controls.Add(this.scrllBannerLabel);
             this.Controls.Add(this.dropDownTxtPos);
             this.Controls.Add(this.bannerAddPicture);
             this.Controls.Add(this.ctaThrdBtnText);
             this.Controls.Add(this.ctaScndBtnText);
             this.Controls.Add(this.ctaFirstBtnText);
-            this.Controls.Add(this.callToActionText);
-            this.Controls.Add(this.bannerTextBox);
             this.Controls.Add(this.bannerCheckBox);
             this.Name = "Form1";
             this.Text = "Page Generator";
@@ -142,14 +158,15 @@ namespace AutoGeneratingPage
         #endregion
 
         private System.Windows.Forms.CheckBox bannerCheckBox;
-        private System.Windows.Forms.TextBox bannerTextBox;
-        private System.Windows.Forms.TextBox callToActionText;
         private System.Windows.Forms.TextBox ctaFirstBtnText;
         private System.Windows.Forms.TextBox ctaScndBtnText;
         private System.Windows.Forms.TextBox ctaThrdBtnText;
         private System.Windows.Forms.Button bannerAddPicture;
         private System.Windows.Forms.ComboBox dropDownTxtPos;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label scrllBannerLabel;
+        private System.Windows.Forms.Label ctaBtnsLabel;
+        private System.Windows.Forms.Label txtWithImagePos;
+        private System.Windows.Forms.TextBox imgText;
     }
 }
 
