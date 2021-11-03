dcae1d18c7561bc6d974cb51f268488e995b1e3e
Daniel Xu <daniel23xu@hotmail.com>
2021-11-03 14:00:53 +0100

    fixed buttons for bottom form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,97 @@ namespace AutoGeneratingPage
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnWithLink = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.checkBoxBtnLink = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnWithLink
+            // 
+            this.btnWithLink.Location = new System.Drawing.Point(26, 377);
+            this.btnWithLink.Name = "btnWithLink";
+            this.btnWithLink.Size = new System.Drawing.Size(98, 35);
+            this.btnWithLink.TabIndex = 0;
+            this.btnWithLink.Text = "text";
+            this.btnWithLink.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(506, 383);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(94, 29);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(635, 383);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(94, 29);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(353, 383);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(94, 29);
+            this.btnPreview.TabIndex = 4;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // checkBoxBtnLink
+            // 
+            this.checkBoxBtnLink.AutoSize = true;
+            this.checkBoxBtnLink.Location = new System.Drawing.Point(26, 337);
+            this.checkBoxBtnLink.Name = "checkBoxBtnLink";
+            this.checkBoxBtnLink.Size = new System.Drawing.Size(141, 24);
+            this.checkBoxBtnLink.TabIndex = 5;
+            this.checkBoxBtnLink.Text = "Button with link?";
+            this.checkBoxBtnLink.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBoxBtnLink);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnWithLink);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnWithLink;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.CheckBox checkBoxBtnLink;
     }
 }
 
