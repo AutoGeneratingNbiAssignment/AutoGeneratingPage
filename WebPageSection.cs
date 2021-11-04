13f8d89461d69f269257268ad502c56ebc508c6b
Vivi Tran <vihanhtranjobb@hotmail.com>
2021-11-04 16:26:57 +0100

    Add webpagesection and webpagesectiontype class

---
+++ WebPageSection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoGeneratingPage
+{
+	class WebPageSection
+	{
+        public WebPageSectionType SectionType { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; } 
+        public string Title { get; set; } 
+        public string FontStyle { get; set; }
+        public string FontSize { get; set; }
+        public string FontColor { get; set; }
+        public string BackgroundColor { get; set; }
+        public string Text { get; set; }
+        public List<string> Images { get; set; }
+       
+    }
+}
---
+++ WebPageSectionType.cs
@@ -0,0 +1,6 @@
+﻿namespace AutoGeneratingPage
+{
+	public class WebPageSectionType
+	{
+	}
+}
